b0afe7ce122d2f0383c781011d4f85452a520f4f
cbansimba <ceulain.bansimba@canal-plus.com>
2021-12-28 18:45:18 +0100

    add structure portfolio

---
+++ components/Container.tsx
@@ -0,0 +1,58 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { FC } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+interface IContainerProps {
+  children: FC;
+}
+
+const Container: FC<IContainerProps> = ({ children }) => {
+  const router = useRouter();
+
+  const meta = {
+    title: "Ceulain Bansimba - Developer and Football Educator",
+    description: `I love football`,
+    image: "/avatar.png",
+    type: "website",
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        {/* <meta name="robots" content="follow, index" /> */}
+        <meta content={meta.description} name="description" />
+        {/* <meta
+          property="og:url"
+          content={`https://yourwebsite.com${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://yourwebsite.com${router.asPath}`}
+        /> */}
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Ceulain Bansimba" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {/* <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} /> */}
+        {/* {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )} */}
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  );
+};
+
+export default Container;
