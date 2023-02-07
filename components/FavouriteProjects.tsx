a4efb128f880e0aec54e5cba2d563097e1ed7162
Ceulain <ceul1barth@hotmail.fr>
2023-02-07 21:10:00 +0100

    feat: add favourite projects

---
+++ components/FavouriteProjects.tsx
@@ -0,0 +1,11 @@
+const FavouriteProject = () => (
+  <div className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900">
+    <div className="max-w-6xl mx-auto lg:ml-20">
+      <h1 className="pt-40 text-6xl lg:text-9xl max-w-lg font-bold text-gray-500  text-center my-20lg:text-leftd:my-0 md:text-white dark:text-gray-600 lg:text-left">
+        Favourite Projects
+      </h1>
+    </div>
+  </div>
+);
+
+export default FavouriteProject;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
+import FavouriteProject from "components/FavouriteProjects";
 import Hero from "components/Hero";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import Container from "../components/Container";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <Container>
-      <Hero />
+      <>
+        <Hero />
+        <FavouriteProject />
+      </>
     </Container>
   );
 };
