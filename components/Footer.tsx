fce50449123e6a52fdec8ca92c7361f96b5c288d
Ceulain <ceul1barth@hotmail.fr>
2024-02-02 23:19:28 +0100

    feat: remove footer element

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,19 +9,6 @@ const Footer: FC = () => {
         <div className="h-0.5 w-full bg-white"></div>
         <div className="flex flex-row space-y-4 md:space-y-0 justify-between mt-8 flex-wrap">
           <p>&copy; {`${userData.name}. All Rights Reserved.`}</p>
-          <div>
-            <p>
-              Developed by
-              <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
-                <a
-                  className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-50"
-                  href="https://manuarora.in"
-                >
-                  Manu Arora
-                </a>
-              </div>
-            </p>
-          </div>
           <div className="space-x-4 flex flex-row items-center">
             <SocialNetwork />
           </div>
